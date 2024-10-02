c9db08408e1b0c51ef4a5307ac641695cb0fdffe
Farhan <151362771+farhan6397@users.noreply.github.com>
2024-10-02 14:30:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate and delete/rotate-and-delete.java
+++ Difficulty: Medium/Rotate and delete/rotate-and-delete.java
@@ -11,9 +11,9 @@ class Solution {
     public static int rotateDelete(ArrayList<Integer> arr) {
         // code here
         int n = arr.size(), temp= n/2;
-   int i=3*(temp/2);
-     if(temp%2==1)i++;
-     return arr.get(n-i);
+        int i=3*(temp/2);
+        if(temp%2==1)i++;
+        return arr.get(n-i);
     }
 }
 
